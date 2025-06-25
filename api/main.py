ae492954e4eb8bb7d6ba9f4101bf29c332945a84
noopur <noopurkarkare@gmail.com>
2025-06-25 12:52:33 +0530

    adding predict function to api

--- api/main.py
+++ api/main.py
@@ -1,8 +1,33 @@
+from distributed import UploadFile, File
 from fastapi import FastAPI
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
 
 app = FastAPI()
+
+MODEL = tf.keras.models.load_model("../model/1.keras")
+CLASS_NAMES = ["Early Blight","Late Blight","Healthy"]
 
 @app.get("/ping")
 async def ping():
     return "Hello i am alive"
 
+def read_file_as_image(data)->np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+        file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    
+    MODEL.predict(image)
+
+    return
+
+if __name__ == "__main__":
+    uvicorn.run(app,host='localhost',port=8000)
